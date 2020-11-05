27ffc77b4227fcd2f26c4ae87897cca68f1860c3
Radhika-Makkar <72884269+Radhika-Makkar@users.noreply.github.com>
2020-11-05 22:53:47 +0530

    Update newapp.js

--- newapp.js
+++ newapp.js
@@ -12,10 +12,35 @@ model: BookModel,
 
 
 });
+window.onload = function(){document.getElementById("top2").style.display = "none"; };
+//window.onload = function(){document.getElementById("SAVE").style.display = "none"; };
+   x=document.querySelector('#top1');
+        z=document.querySelector('#top2');
+  y=document.querySelector('#bottom');
 
+x.addEventListener('click', (e)=>{
+if(e.target.innerHTML == "ADD BOOK")
+{
+	e.preventDefault();
+  e2=e.target.parentElement.parentElement;
+   console.log(e2);
+     x.style.display = "none";
+   document.getElementById("top2").style.display = "block";
+ document.getElementById("bottom").style.display = "none";
+}});
 
+z.addEventListener('click', (e)=>{
+	e.preventDefault();
+	console.log(e.target.innerHTML);
+if(e.target.innerHTML == "Add Book")
+{
+  e2=e.target.parentElement.parentElement;
 
+    x.style.display = "block";
+   document.getElementById("top2").style.display = "none";
+      document.getElementById("bottom").style.display = "block";
 
+}});
 var BookRecordsView = Backbone.View.extend({
 //The body element in which all elements are displayed
 el: 'body',
@@ -25,8 +50,8 @@ events:{
 'click button#btnadd': 'addData',
 'click button#btnclear': 'clearInput',
 'click button.delete' : 'deleteBook',
-'click button#edit' :'updateBook',
-'click button#save' : 'saveBook',
+'click button#EDIT' :'updateBook',
+'click button#SAVE' : 'saveBook',
 'click button#view' :'viewBook'
 },
 deleteBook:function(e)
@@ -36,21 +61,23 @@ deleteBook:function(e)
  },
 updateBook:function(e)
 {
+//console.log("hello");
+var button1=document.getElementById("EDIT").style.display="none";
+var button2=document.getElementById("SAVE").style.display="block";
+ var title=e.target.parentElement.querySelector("#one");
 
- var title=e.target.parentElement.parentElement.querySelector("#TITLE");
+  var author=e.target.parentElement.querySelector("#two");
 
-  var author=e.target.parentElement.parentElement.querySelector("#AUTHOR");
-   var isbn=e.target.parentElement.parentElement.querySelector("#ISBN");
-     var image=e.target.parentElement.parentElement.querySelector("#IMAGE");
-
+ var isbn=e.target.parentElement.querySelector("#three");
+ var image=e.target.parentElement.querySelector("#four");
  var title_data=title.innerHTML;
  var author_data=author.innerHTML;
  var isbn_data=isbn.innerHTML;
-	 var image_data=image.innerHTML;
+	  // var image_data=image.innerHTML;
 	 title.innerHTML="<input type='text' id='title_text' value='"+title_data+"'>";
  author.innerHTML="<input type='text' id='author_text' value='"+author_data+"'>";
  isbn.innerHTML="<input type='text' id='isbn_text' value='"+isbn_data+"'>";
-  image.innerHTML="<input type='text' id='image_text' value='"+image_data+"'>";
+   // image.innerHTML="<input type='text' id='image_text' value='"+image_data+"'>";
 },
 saveBook:function(e)
 {
@@ -62,44 +89,75 @@ saveBook:function(e)
 
 var author_val=document.getElementById("author_text").value;
 var isbn_val=document.getElementById("isbn_text").value;
-var image_val=document.getElementById("image_text").value;
+// var image_val=document.getElementById("image_text").value;
 
-// console.log(e.target.parentElement.parentElement.querySelector(('#TITLE')))
-// console.log(title_val)
 e.target.parentElement.parentElement.querySelector(('#TITLE')).innerHTML=title_val;
- // console.log(title_val);
- // console.log(author_val);
- // console.log(isbn_val);
  e.target.parentElement.parentElement.querySelector(('#AUTHOR')).innerHTML=author_val;
  
   e.target.parentElement.parentElement.querySelector(('#ISBN')).innerHTML=isbn_val;
-    e.target.parentElement.parentElement.querySelector(('#IMAGE')).innerHTML=image_val;
-
-  // document.getElementById("AUTHOR").innerHTML=author_val;
-  //  document.getElementById("ISBN").innerHTML=isbn_val;
+ 
+  document.getElementById("EDIT").style.display="block";
+ document.getElementById("SAVE").style.display="none";
 
 
 },
-  render: function () {
+  render: function (e) {
   
     //conole.log("hello");
-var dvcontainer = $(this.el).find('#dvcontainer');
-var viewHtml = '<table border="1" class="table table-striped table-light mt-5">';
-viewHtml += "<tr><td>TITLE</td><td>AUTHOR</td><td>ISBN</td><td>IMAGE ADD</td><td>DELETE</td><td>EDIT</td><td>SAVE</td><td>VIEW</td></tr>";
+var bottom = $(this.el).find('#bottom');
+console.log(bottom);
+var card = document.createElement('div');
+  card.setAttribute('class', 'card col-sm-12 col-md-4 col-lg-3');
+
 //Iterate through the collection
 _.each(this.collection.models, function (m, i, l) {
 	
-    var bookRecHtml = '<tr><td id="TITLE">' + m.get('TITLE') + '</td><td id="AUTHOR">' + m.get('AUTHOR') + '</td><td id="ISBN">' + m.get('ISBN')+'</td><td id="img"><img height="100" width="100"src="'+m.get('IMAGE')+'"> </td><td>'+ '<button class="btn btn-info btn-xl delete">X</button>'+
-    '</td><td>'+ '<button class="btn btn-info btn-xl" id="edit">EDIT</button>'+'</td><td>'+ '<button class="btn btn-info btn-xl" id="save">SAVE</button>'+'</td><td>'+ '<button class="btn btn-info btn-xl" id="view" >VIEW</button>'+'</td></tr>';
-    viewHtml += bookRecHtml;
+const app = document.getElementById('bottom');
+const container = document.createElement('div');
+container.setAttribute('class', 'row');
+
+app.appendChild(container);
+  const img = document.createElement('img'); 
+  img.setAttribute('id','four');
+  img.setAttribute('width','100');
+   img.setAttribute('height','100');
+      img.src = m.get('IMAGE');
+      
+      const h1 = document.createElement('p')
+      h1.setAttribute('id','one');
+      h1.textContent = `TITLE: `+m.get('TITLE');
+      // console.log(h1.textContent);
+  const h2 = document.createElement('p')
+      h2.setAttribute('id','two');
+      h2.textContent = `AUTHOR: `+m.get('AUTHOR');
+        const h3 = document.createElement('p')
+      h3.setAttribute('id','three');
+      h3.textContent = `ISBN: `+m.get('ISBN');
+ b=document.createElement('button');
+    b.setAttribute('id','EDIT');
+
+    b.setAttribute('class','btn btn-primary');
+    b.textContent="EDIT";
+  
+     b1=document.createElement('button');
+    b1.setAttribute('id','SAVE');
+
+    b1.setAttribute('class','btn btn-primary');
+    b1.textContent="SAVE";
+// console.log(b1.textContent);
+ container.appendChild(card)
+ card.appendChild(img);
+   card.appendChild(h1);
+      card.appendChild(h2);
+      card.appendChild(h3);
+      card.appendChild(b);
+  card.appendChild(b1);
 
 });
-viewHtml += '</table>';
-dvcontainer.html(viewHtml);
-
 },   
  
 addData: function (e) {
+
   e.preventDefault();
 var newBook = new BookModel();
 newBook.set('TITLE', $("#TITLE").val());
@@ -111,93 +169,26 @@ newBook.set('IMAGE', $("#IMAGE").val());
     $("#ISBN").val("");
      $("#IMAGE").val("");
 BookCollection.add(newBook);
-  localStorage.setItem("BooksCollection", JSON.stringify(BooksCollection));
+BookCollection.reset();
+
 },  
 
 
-//Function to clear all textboxes.
-clearInput: function (e) {
-  e.preventDefault();
-//Clear all Textboxes
-$("#book-form input").val('');
-},
+// //Function to clear all textboxes.
+// clearInput: function (e) {
+//   e.preventDefault();
+// //Clear all Textboxes
+// $("#book-form input").val('');
+// },
 
 viewBook:function(e)
 {
 
-       x=document.querySelector('#top');
+       x=document.querySelector('#top1');
+        z=document.querySelector('#top2');
   y=document.querySelector('#bottom');
-   z=document.querySelector('.row');
-
-// console.log(x);
-
-x.addEventListener('click', (e)=>{
-if(e.target.innerHTML === "VIEW")
-{
-  e2=e.target.parentElement.parentElement;
-  // console.log(e2);
-  // console.log(e.target.parentElement);
-
-    x.style.display = "none";
-    y.style.display="block";
-    
-   
-
-    b=document.createElement('button');
-    b.setAttribute('id','btn');
-    b.setAttribute('class','btn btn-success');
-    b.setAttribute("style", "display:block;margin:0 auto");
-    b.textContent="GO BACK";
-
-      // const h2 = document.createElement('p')
-      // h2.setAttribute('id','two');
-      // h2.textContent =`Capital:  ${country.capital}`;
- const  h1 = document.createElement("h1");
-   h1.setAttribute("class", "display-4");
-   h1.setAttribute("style", "text-align:center");
-   h1.innerHTML = e.target.parentElement.parentElement.querySelector("#TITLE").innerHTML;
-
- const  h2 = document.createElement("h1");
-   h2.setAttribute("class", "display-4");
-   h2.setAttribute("style", "text-align:center");
-   h2.innerHTML = e.target.parentElement.parentElement.querySelector("#AUTHOR").innerHTML;
-
- const h3 = document.createElement("h1");
-   h3.setAttribute("class", "display-4");
-   h3.setAttribute("style", "text-align:center");
-   h3.innerHTML = e.target.parentElement.parentElement.querySelector("#ISBN").innerHTML;
-
- const img = e2.querySelector("img").src;
- // console.log(img);
-   const image = document.createElement("IMG");
-   image.setAttribute("id", "img2");
-
-   image.src = img;
- 
- 
-
-  
-y.appendChild(image);
-  y.appendChild(h1);
-  y.appendChild(h2);
-  y.appendChild(h3);
-   y.appendChild(b);
-
-  document.querySelector('#btn').addEventListener('click', (e) => 
-{
-  
 
 
-
-    
-    if (x.style.display === 'none') {
-      x.style.display = 'block'
-  
-    } 
-    y.innerHTML="";
-   
-});
-}});
 }});
 //The Collection Object
 var BookCollection = new BooksCollection();
@@ -209,7 +200,5 @@ collection:BookCollection
 //When Collection changed
 //Render the Updated Table View
 BookCollection.on('add', function () {
- 
-
 BooksView.render();
 });
